66cdbef44413f16edb425f97ac45eb4091679a9e
ZZW <45614754+zzwmagic@users.noreply.github.com>
2022-09-09 17:23:44 +0800

    Add files via upload

---
+++ Console/20220824Property/20220824Property/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220824Property
+{
+
+    public class info
+    {
+        private string _AA = "hello";
+        public string AA
+        {
+            get { return _AA; }
+            set { _AA = value; }
+        }
+    }
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+            info _info = new info();
+            Console.WriteLine(_info.AA);
+            Console.ReadKey();
+        }
+    }
+}
